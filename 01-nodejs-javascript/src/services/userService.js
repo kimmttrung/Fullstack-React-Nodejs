6d24a94bdee3b1af5b49860025123a75a7d12bc8
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-05-03 21:36:21 +0700

    api register and hashPassword

---
+++ 01-nodejs-javascript/src/services/userService.js
@@ -0,0 +1,32 @@
+const User = require("../models/user");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const createUserService = async (name, email, password) => {
+    try {
+        // hash user password
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+
+        // save user to database
+        let result = await User.create({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: "USER"
+
+        })
+        return result;
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+
+
+
+
+module.exports = {
+    createUserService
+}
