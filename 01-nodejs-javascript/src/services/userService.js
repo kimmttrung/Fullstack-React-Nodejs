c2900b9cb0d68c9aa3bdc2224695a6334fbdaa57
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-05-04 00:02:03 +0700

    json web token

--- 01-nodejs-javascript/src/services/userService.js
+++ 01-nodejs-javascript/src/services/userService.js
@@ -1,5 +1,9 @@
+require("dotenv").config(); // use env
+
 const User = require("../models/user");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const saltRounds = 10;
 
 const createUserService = async (name, email, password) => {
@@ -36,7 +40,24 @@ const handleLoginService = async (email1, password) => {
                 }
             } else {
                 // create an access token
-                return ('create an access token')
+                const payload = {
+                    name: user.name,
+                    email: user.email
+                }
+                const access_token = jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    {
+                        expiresIn: process.env.JWT_EXPIRE
+                    }
+                )
+                return {
+                    access_token,
+                    user: {
+                        email: user.email,
+                        name: user.name
+                    }
+                };
 
             }
         } else {
