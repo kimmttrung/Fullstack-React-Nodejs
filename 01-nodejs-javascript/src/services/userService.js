57822051ba2d6c47225f97d891fc7ef4145de74c
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-05-03 23:39:00 +0700

    api login

--- 01-nodejs-javascript/src/services/userService.js
+++ 01-nodejs-javascript/src/services/userService.js
@@ -22,11 +22,54 @@ const createUserService = async (name, email, password) => {
         return null;
     }
 }
+const handleLoginService = async (email1, password) => {
+    try {
+        // fetch user by email
+        const user = await User.findOne({ email: email1 })
+        if (user) {
+            // compare pasword
+            const isMatchPassword = await bcrypt.compare(password, user.password);
+            if (!isMatchPassword) {
+                return {
+                    EC: 2,
+                    EM: "Password không hợp lệ"
+                }
+            } else {
+                // create an access token
+                return ('create an access token')
+
+            }
+        } else {
+            return {
+                EC: 1,
+                EM: "Email password không hợp lệ"
+            }
+        }
+
+
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+
+        // save user to database
+        let result = await User.create({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: "USER"
+
+        })
+        return result;
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
 
 
 
 
 
 module.exports = {
-    createUserService
+    createUserService,
+    handleLoginService
 }
