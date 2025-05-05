db808a538b86c5c03a069c02beddd494cbcf2bd9
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-05-05 17:03:13 +0700

    middleware of nodejs

--- 01-nodejs-javascript/src/routes/api.js
+++ 01-nodejs-javascript/src/routes/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createUser, handleLogin, getAllUser } = require('../controllers/userController');
+const delay = require('../middleware/delay');
 
 const routerAPI = express.Router();
 
-
+routerAPI.all("*", delay);
 routerAPI.get('/', (req, res) => {
     return res.status(200).json("Hello world api");
 })
