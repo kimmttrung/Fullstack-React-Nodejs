4074ca44e5c875ef3ccbaa21a26622eb9e4ecde2
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-05-06 16:45:38 +0700

    react context api

--- 00-react-javascript/src/components/layout/header.jsx
+++ 00-react-javascript/src/components/layout/header.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/auth.context';
 
 const Header = () => {
     const navigate = useNavigate();
+    const { auth, setAuth } = useContext(AuthContext);
+    console.log(">>> cehck auth", auth);
 
     const items = [
         {
@@ -12,28 +15,40 @@ const Header = () => {
             key: 'home',
             icon: <MailOutlined />,
         },
+        ...(auth?.isAuthenticated ? [
+            {
+                label: <Link to={'/user'}>User</Link>,
+                key: 'user',
+                icon: <MailOutlined />,
+            }
+        ] : []),
+
         {
-            label: <Link to={'/user'}>User</Link>,
-            key: 'user',
-            icon: <MailOutlined />,
-        },
-        {
-            label: 'Welcome my project',
+            label: `Welcome ${auth?.user?.name}`,
             key: 'SubMenu',
             icon: <SettingOutlined />,
             children: [
-                {
+                ...(auth?.isAuthenticated ? [
+                    {
+                        label: <span onClick={() => {
+                            localStorage.clear("access_token");
+                            setAuth({
+                                isAuthenticated: false,
+                                user: {
+                                    email: "",
+                                    name: "",
+                                }
+                            })
+                            navigate("/");
+                            setCurrent("home");
+                        }}>Đăng xuất</span>,
+                        key: 'logout',
+                    }
+                ] : [{
                     label: <Link to={'/login'}>Đăng nhập</Link>,
                     key: 'login',
-                },
-                {
-                    label: <span onClick={() => {
-                        localStorage.clear("access_token");
-                        navigate("/");
-                        setCurrent("home");
-                    }}>Đăng xuất</span>,
-                    key: 'logout',
-                },
+                }]
+                ),
             ],
         },
     ];
--- 00-react-javascript/src/pages/login.jsx
+++ 00-react-javascript/src/pages/login.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Form, Input, notification } from 'antd';
 import { loginApi } from '../utils/api';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../components/context/auth.context';
 
 const LoginPage = () => {
     const navigate = useNavigate();
+    const { setAuth } = useContext(AuthContext);
     const onFinish = async (values) => {
         const { email, password } = values;
 
@@ -16,6 +18,13 @@ const LoginPage = () => {
                 message: "LOGIN USER",
                 description: "Success"
             });
+            setAuth({
+                isAuthenticated: true,
+                user: {
+                    email: res?.user?.email ?? "",
+                    name: res?.user?.name ?? "",
+                }
+            })
             navigate('/');
         } else {
             notification.error({
